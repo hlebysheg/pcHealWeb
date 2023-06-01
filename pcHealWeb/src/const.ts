dd26d625b85af09e7bedb8b4a24a23529e1b90e2
Александр <savchic2000@mail.ru>
2023-06-01 22:40:48 +0300

    fix-acces-control

--- pcHealWeb/src/const.ts
+++ pcHealWeb/src/const.ts
@@ -1,1 +1,2 @@
-export const host = "http://localhost:5000";
+export const host = "http://localhost:8000";
+export const host_hub = "http://localhost:8000";
--- pcHealWeb/src/pages/pcinfo.tsx
+++ pcHealWeb/src/pages/pcinfo.tsx
@@ -1,49 +1,46 @@
-import { HubConnectionBuilder } from "@microsoft/signalr";
+import signalR, { HubConnectionBuilder } from "@microsoft/signalr";
 import { useEffect, useState } from "react";
 import { $token } from "../features/login";
 import { useStore } from "effector-react";
-import { host } from "../const";
+import { host, host_hub } from "../const";
 type PCInfoMessage = {
-    cPUName:string
-    cPUTemp:number
-    cPULoad:number
-    cPUFrenq:number
+  cpuName: string;
+  cpuTemp: number;
+  cpuLoad: number;
+  cpuFrenq: number;
 
-    gPUName:string
-    gPUTemp:number
-    gPULoad:number
-}
+  gpuName: string;
+  gpuTemp: number;
+  gpuLoad: number;
+};
 export const PcInfoPage = () => {
-    const [connection, setConnection] = useState<any>();
-    const tokens = useStore($token)
-    useEffect(() => {
-        const connect = new HubConnectionBuilder()
-            .withUrl(host+"/api/pchealh/hub", {
-                accessTokenFactory: () => {
-                    return `${tokens.accesToken}`
-                }
-            })
-            .withAutomaticReconnect()
-            .build();
-            connect.start()
-            console.log(connect)
-        setConnection(connect);
-    }, [])
+  const [connection, setConnection] = useState<any>();
+  const tokens = useStore($token);
+  useEffect(() => {
+    const connect = new HubConnectionBuilder()
+      .withUrl(host_hub + "/api/pchealh/hub", {
+        accessTokenFactory: () => {
+          return `${tokens.accesToken}`;
+        },
+        withCredentials: false,
+      })
+      .withAutomaticReconnect()
+      .build();
+    connect.start();
+    console.log(connect);
+    setConnection(connect);
+  }, []);
 
-    useEffect(() => {
-        if(connection == undefined){
-            return;
-        }
-        connection.on("Notify", (msg: string) => {
-            console.log(msg);
-        })
-        connection.on("Receive", (info: PCInfoMessage) => {
-            console.log(info);
-        })
-    }, [connection])
-    return (
-        <div>
-            pcinfo
-        </div>
-    )
-}
+  useEffect(() => {
+    if (connection == undefined) {
+      return;
+    }
+    connection.on("Notify", (msg: string) => {
+      console.log(msg);
+    });
+    connection.on("PCInfo", (info: PCInfoMessage) => {
+      console.log(info);
+    });
+  }, [connection]);
+  return <div>pcinfo</div>;
+};
